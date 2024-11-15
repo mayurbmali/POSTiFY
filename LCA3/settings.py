49af879e07ed493f86234d63f78574ffee29be01
Mayur Mali <mayurbmali80@gmail.com>
2024-11-15 16:04:20 +0530

    Initial commit: Add Django project files

--- LCA3/settings.py
+++ LCA3/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1ik(a#+fcp!y00y%)po8u%=+3_@$=vv49!^ln3&jsh1855p!q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
