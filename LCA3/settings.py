ea40a047b137ab6fc73a1992b7dd541d4c206a7d
Mayur Mali <mayurbmali80@gmail.com>
2024-11-15 16:48:50 +0530

    Initial commit: Add Django project files

--- LCA3/settings.py
+++ LCA3/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-1ik(a#+fcp!y00y%)po8u%=+3_@$=vv49!^ln3&jsh1855p!q$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
