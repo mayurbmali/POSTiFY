3f93676442f09f324ecf749873623c515b69eba3
Mayur Mali <mayurbmali80@gmail.com>
2024-11-16 03:01:49 +0530

    Testing

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
-# exit on error
+# Exit on error
 set -o errexit
 
+# Activate the virtual environment
+source C:/Users/Mayur Mali/djangoEnv/Scripts/activate
+
+# Install dependencies
 pip3 install -r requirements.txt
 
-python3.9 manage.py collectstatic --noinput 
+# Run Django management commands
+python3.9 manage.py collectstatic --noinput
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
-if [[ $CREATE_SUPERUSER ]];
-then
+
+# Create superuser if required
+if [[ $CREATE_SUPERUSER ]]; then
   python3.9 manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL"
 fi
