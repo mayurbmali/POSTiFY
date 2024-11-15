edd682be71f6177b5339db3cded2bd688eb6e08d
Mayur Mali <mayurbmali80@gmail.com>
2024-11-16 02:53:24 +0530

    Testing

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 # exit on error
 set -o errexit
 
-python pip3 install -r requirements.txt
+python3.9 pip3 install -r requirements.txt
 
-python manage.py collectstatic --noinput 
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python3.9 manage.py collectstatic --noinput 
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL"
+  python3.9 manage.py createsuperuser --noinput --email "$DJANGO_SUPERUSER_EMAIL"
 fi
